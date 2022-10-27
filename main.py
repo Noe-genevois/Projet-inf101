004766393777ce60f4bf3a3d1fb6a1933422570a
Noe-genevois <noe.genevois@gmail.com>
2022-10-27 21:05:33 +0200

    suite

--- main.py
+++ main.py
@@ -12,3 +12,29 @@ for ligne in laby:
     print("\n")
 print("Entrée:",entree)
 print("Sortie:",sortie)
+
+#Stockage de toutes les informations du fichier dans un dictionnaire
+dicoJeu = {"laby":laby,"entrée":entree,"sortie":sortie}
+
+#Fonctions pour l'affichage du labyrinthe
+def afficheTextuel(dicoJeu:dict):
+    """Affichage textuel du labyrinthe dans dicoJeu, '#'=mur 'x'=entrée 'o'=sortie ' '=passage"""
+    laby_liste = dicoJeu["laby"]
+    hauteur = len(laby_liste)
+    largeur = len(laby_liste[0])
+    #on parcours chaque case du labyrinthe
+    for y in range(hauteur):
+        for x in range(largeur):
+            case = laby_liste[y][x]
+            if case == 1: #la case est un mur
+                print('#', end='')
+            else: #case == 0, la case est un passage
+                if dicoJeu["entrée"] == [y,x]:
+                    print('x', end='')
+                elif dicoJeu["sortie"] == [y,x]:
+                    print('o', end='')
+                else: # la case est un passage quelconque
+                    print(' ',end='')
+        print() #On change de ligne à afficher
+
+afficheTextuel(dicoJeu)
